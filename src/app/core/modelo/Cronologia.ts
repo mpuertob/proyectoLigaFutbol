50c212dca34e65a11c76c6004738d386c917fe83
programacion <49229873+mpuertob@users.noreply.github.com>
2020-12-08 12:42:53 +0100

    cronologia cambio local por nombre

--- src/app/core/modelo/Cronologia.ts
+++ src/app/core/modelo/Cronologia.ts
@@ -1,16 +1,26 @@
 import { Partido } from "./Partido";
 
 export class Cronologia {
+  private _nombreLocal: String;
+  private _nombreVisitante: String;
   private _golesCasa: number;
   private _minutosCasa: Array<number>;
   private _golesFuera: number;
   private _minutosFuera: Array<number>;
 
   constructor(private partido: Partido) {
+    this._nombreLocal = partido.local;
+    this._nombreVisitante = partido.visitante;
     this._golesCasa = this.partido.golesLocal;
     this._minutosCasa = this.partido.minutosGolesLocal;
     this._golesFuera = this.partido.golesVisitante;
     this._minutosFuera = this.partido.minutosGolesVisitante;
+  }
+  public get nombreLocal() {
+    return this._nombreLocal;
+  }
+  public get nombreVisitante() {
+    return this._nombreVisitante;
   }
   public get golesCasa() {
     return this._golesCasa;
